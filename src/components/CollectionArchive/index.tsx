e8a0113ebadb5b5d085a39337aaa868b2c11eceb
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2025-03-26 19:30:07 -0400

    Removes comments, add temp logo

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -19,8 +19,6 @@ export const CollectionArchive: React.FC<Props> = (props) => {
             if (typeof result === 'object' && result !== null) {
               return (
                 <div className="col-span-full" key={index}>
-                  // TODO: solve for memorials collection archive - may need separate for posts and
-                  memorials
                   <Card className="h-full" doc={result} relationTo="memorials" showCategories />
                 </div>
               )
